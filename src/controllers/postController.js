8ec465e90c8ca00ccbddfab3dc6b4c2af8c66608
junior <j-unyor14@hotmail.com>
2022-07-25 23:30:36 -0300

    create  endpoint GET /post/:id

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,15 +1,36 @@
 const { BlogPost, User, Category } = require('../database/models');
 
 const getPost = async (req, res) => {
-  const post = await BlogPost.findAll({ include: [
-    { model: User, as: 'user', attributes: { exclude: 'password' } },
-    { model: Category, as: 'categories', through: { attributes: [] } },
-  ],
-});
+  const post = await BlogPost.findAll({
+    include: [
+      { model: User, as: 'user', attributes: { exclude: 'password' } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+
+  return res.status(200).json(post);
+};
+
+const getPostId = async (req, res) => {
+  const { id } = req.params;
+
+  const post = await BlogPost.findByPk(
+    id, {
+      include: [
+        { model: User, as: 'user', attributes: { exclude: 'password' } },
+        { model: Category, as: 'categories', through: { attributes: [] } },
+      ],
+  },
+);
+
+  if (!post) {
+    return res.status(404).json({ message: 'Post does not exist' });
+  }
 
   return res.status(200).json(post);
 };
 
 module.exports = {
   getPost,
+  getPostId,
 };
--- src/routes/routers.js
+++ src/routes/routers.js
@@ -2,64 +2,54 @@ const express = require('express');
 const { body } = require('express-validator');
 const controller = require('../controllers/userController');
 const controllerLogin = require('../controllers/loginController');
-const { createCategories, getCategories } = require('../controllers/categoriesController');
-const { getPost } = require('../controllers/postController');
+const {
+  createCategories,
+  getCategories,
+} = require('../controllers/categoriesController');
+const { getPost, getPostId } = require('../controllers/postController');
 const validate = require('../middleware/validation');
 const jwt = require('../helpers/jwt');
 
 const router = express.Router();
 
 router
-.route('/login')
-.post(
-  body('email').isEmail(),
-  body('password').isLength({ min: 5 }),
-  validate.validateFields,
-  validate.validateLogin,
-  controllerLogin,
-);
-
-router
-.route('/user/:id')
-.get(
-  jwt.verifyToken,
-  validate.validateUserId,
-  controller.getUserId,
-);
-
-router
-.route('/user')
-.get(
-  jwt.verifyToken,
-  controller.getUsers,
-  )
-.post(
-  body('email').isEmail(),
-  body('displayName').isLength({ min: 8 }),
-  body('password').isLength({ min: 6 }),
-  validate.validateUser,
-  validate.validateEmailExist,
-  controller.controllerUser,
+  .route('/login')
+  .post(
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+    validate.validateFields,
+    validate.validateLogin,
+    controllerLogin,
   );
 
 router
-.route('/categories')
-.post(
-  body('name').isLength({ min: 1 }),
-  jwt.verifyToken,
-  validate.validateCategoryName,
-  createCategories,
-)
-.get(
-  jwt.verifyToken,
-  getCategories,
-);
+  .route('/user/:id')
+  .get(jwt.verifyToken, validate.validateUserId, controller.getUserId);
 
 router
-.route('/post')
-.get(
-  jwt.verifyToken,
-  getPost,
-);
+  .route('/user')
+  .get(jwt.verifyToken, controller.getUsers)
+  .post(
+    body('email').isEmail(),
+    body('displayName').isLength({ min: 8 }),
+    body('password').isLength({ min: 6 }),
+    validate.validateUser,
+    validate.validateEmailExist,
+    controller.controllerUser,
+  );
+
+router
+  .route('/categories')
+  .post(
+    body('name').isLength({ min: 1 }),
+    jwt.verifyToken,
+    validate.validateCategoryName,
+    createCategories,
+  )
+  .get(jwt.verifyToken, getCategories);
+
+router.route('/post').get(jwt.verifyToken, getPost);
+
+router.route('/post/:id').get(jwt.verifyToken, getPostId);
 
 module.exports = router;
